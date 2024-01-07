c6729bf508964358ffa00588587077fc0e24590a
grgsailesh85 <143858404+grgsailesh85@users.noreply.github.com>
2024-01-07 17:41:47 +0545

    abc

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,4 +1,4 @@
-const resultElements = document.getElementById('result')
+const resultElement = document.getElementById('result')
 const clearBtn = document.getElementById('clear-button');
 const deleteBtn = document.getElementById('delete-button');
 const divideBtn = document.getElementById('divide-button');
@@ -8,3 +8,26 @@ const addBtn = document.getElementById('add-button');
 const decimalBtn = document.getElementById('decimal-button');
 const equalBtn = document.getElementById('equal-button');
 const numberBtns=document.querySelectorAll('.number');
+
+//Initialize the variables
+let result ='';
+let operation='';
+let previousOperand = 0;
+
+
+
+// Function to append numnber
+const appendNumber = (number) => {
+  result += number;
+  resultElement.innerText=result;
+}
+
+
+
+
+//Add event listener to number buttons
+numberBtns.forEach(button => {
+  button.addEventListener('click',() =>{
+    appendNumber(button.innerText);
+  });
+});
