54d6370b7676fdae7e8e8699d82fd6700462c9e5
grgsailesh85 <143858404+grgsailesh85@users.noreply.github.com>
2024-01-07 16:01:11 +0545

    updated

---
+++ javascripts/scripts.js
@@ -0,0 +1,10 @@
+const resultElements = document.getElementById('result')
+const clearBtn = document.getElementById('clear-button');
+const deleteBtn = document.getElementById('delete-button');
+const divideBtn = document.getElementById('divide-button');
+const multiplyBtn = document.getElementById('multiply-button');
+const subtractBtn = document.getElementById('subtract-button');
+const addBtn = document.getElementById('add-button');
+const decimalBtn = document.getElementById('decimal-button');
+const equalBtn = document.getElementById('equal-button');
+const numberBtns=document.querySelectorAll('.number');
